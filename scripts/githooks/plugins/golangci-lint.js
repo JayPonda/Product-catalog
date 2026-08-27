7b0770bed083cdd1d75d299681b168dc776ac2d5
jponda <pondajay637@gmail.com>
2026-08-27 17:57:48 +0530

    refactor: parallel frontend/backend CI jobs and upgrade golangci-lint to v2

--- scripts/githooks/plugins/golangci-lint.js
+++ scripts/githooks/plugins/golangci-lint.js
@@ -1,6 +1,6 @@
 module.exports = (engine) => {
   engine.registerPlugin('golangci-lint', (e, dir, files) => {
-    const success = e.runCommand(dir, 'go', ['run', 'github.com/golangci/golangci-lint/cmd/golangci-lint@v1.64.5', 'run', './...']);
+    const success = e.runCommand(dir, 'go', ['run', 'github.com/golangci/golangci-lint/v2/cmd/golangci-lint@v2.13.1', 'run', './...']);
     if (!success) {
       throw new Error('Go linting failed');
     }
